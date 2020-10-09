993223749f925a0d87903de1b2b7ecb8279bc7c5
xomgoth <58498084+xomgoth@users.noreply.github.com>
2020-10-09 22:47:40 +0700

    Revert "Lap5-ph09328"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\Student;
-use App\Models\Subject;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -15,11 +14,5 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // User::factory(10)->create();
-        // $this ->  call([
-        //     StudentsTableSeeder::class,
-        //     SubjectsTableSeeder::class,
-        // ]);
-        Student::factory(10)->create();
-        Subject::factory(20)->create();
     }
 }
